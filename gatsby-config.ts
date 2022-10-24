ce4eb2d5b780f15016a17c6eaa2153e7be5e4f47
Steffen Kühne <stekhn@gmail.com>
2022-10-24 21:20:05 +0200

    Add path prefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: "Gatsby Dark Mode Demo",
-    siteUrl: "https://github.com/stekhn/gatsby-dark-mode",
-  },
+  pathPrefix: `/gatsby-dark-mode`,
   graphqlTypegen: false,
   plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-use-dark-mode"],
 };
